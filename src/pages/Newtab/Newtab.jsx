54cd137963eac072b5d44ac2abab361773450204
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-08-12 03:11:35 -0500

    Updated the home page when you open a new tab in chrome

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
+  const [urlList, setUrlList] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [tabID, setTabID] = useState(0);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    setTabID(tabs[0].id);
+  });
+  const subredditList = ['wholesomememes', 'Eyebleach', 'aww'];
+  useEffect(() => {
+    if (tabID !== 0) {
+      const getRedditData = async () => {
+        const redditData = await axios.get(
+          `https://www.reddit.com/r/${
+            subredditList[Math.floor(Math.random() * 2)]
+          }/.json?count=50`
+        );
+        const items = redditData?.data?.data?.children;
+        const urlList = items
+          .map((item) => item.data.url ?? '')
+          .filter(
+            (item) =>
+              item !== '' &&
+              !item.includes('gfycat') &&
+              /\.(jpe?g|tiff?|png|webp|bmp)$/i.test(item)
+          );
+        setUrlList(urlList);
+      };
+      getRedditData();
+    }
+  }, [tabID]);
+  const rand = Math.floor(Math.random() * 10);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
+        {
+          <img
+            style={
+              loaded
+                ? {
+                    width: '400px',
+                    borderRadius: '25px',
+                  }
+                : { display: 'none' }
+            }
+            src={urlList[urlList.length === 1 ? 0 : rand]}
+            onLoad={() => setLoaded(true)}
+          />
+        }
       </header>
     </div>
   );
