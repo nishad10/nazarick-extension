5e6ca984c381a5124d14768585173472593acd79
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-08-12 04:17:17 -0500

    Added a basic button that returns hashed value of url

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import './Popup.css';
 
 const Popup = () => {
+  const [tabUrl, setTabUrl] = useState();
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    ([currentTab]) => {
+      setTabUrl(currentTab.url);
+    }
+  );
+  const shortenUrl = () => {
+    console.log('in');
+    const hash = axios.post('http://127.0.0.1:5000/shorten', {
+      url: tabUrl,
+    });
+    hash.then((response) => {
+      console.log(response);
+      copyToClipboard(response.data.short);
+    });
+  };
+  const copyToClipboard = (text) => {
+    var dummy = document.createElement('input');
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          style={{
+            background: '#1dd8e4',
+            borderRadius: '11px',
+            padding: '20px 45px',
+            color: '#ffffff',
+            display: 'inline-block',
+            font: 'normal bold 20px/1 "Roboto", sans-serif',
+            textAlign: 'center',
+          }}
+          onClick={() => (tabUrl ? shortenUrl(tabUrl) : null)}
         >
-          Learn React
-        </a>
+          Shorten URL!
+        </button>
       </header>
     </div>
   );
