fa180c425c25dea010b91564eee58a4820469ad6
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-08-16 06:48:26 -0500

    Firebase init

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,25 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
+import secrets from 'secrets';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+const firebaseConfig = {
+  apiKey: secrets.APIKEY,
+  authDomain: secrets.AUTHDOMAIN,
+  databaseURL: secrets.DATABASEURL,
+  projectId: secrets.PROJECTID,
+  storageBucket: secrets.STORAGEBUCKET,
+  messagingSenderId: secrets.MESSAGINGSENDERID,
+  appId: secrets.APPID,
+  measurementId: secrets.MEASUREMENTID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = app.database();
+const db = app.firestore();
+console.log(db.collection('nazarick-ffc4a').get());
+console.log(database.app);
